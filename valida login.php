28c38f1e30a91df76714e43774b1f012e6f267dc
João Pedro <joaopedro453@hotmail.com>
2019-11-01 18:31:56 -0300

    autenticação

--- valida login.php
+++ valida login.php
@@ -1,12 +1,33 @@
 <?php
 
+$_POST['email'];
+$_POST['password'];
+
+$usuarios_app = [
+    ['email' => 'joaopedro453@hotmail.com', 'password' => '123'],
+    ['email' => 'teste@app.com', 'password' => '1'],
+    ['email' => 'teste2@app.com', 'password' => '22'],
+    ['email' => 'teste3@app.com', 'password' => '333'],
+
+];
+
+$usuario_autenticado = false;
+
+foreach ($usuarios_app as $user) {
+    if ($user['email'] == $_POST['email'] && $user['password'] == $_POST['password']) {
+      $usuario_autenticado = true;
+    }
+};
+
+
+if ($usuario_autenticado) {
+    echo 'aaeeee';
+} else {
+    header('Location: index.php?login=erro');
+}
+
 // $_GET['email'];
 // $_GET['password'];
 // print_r($_GET);
 
-print_r($_POST);
-$_POST['email'];
-$_POST['password'];
 
-
-?>
